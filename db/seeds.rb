b6089abb004c785103f480f770380d04a7fd5295
Leanne Egan <lmegan92@gmail.com>
2018-08-08 17:40:59 +0800

    worked on controller and fixed sign up

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.new(first_name: "Lady", last_name: "Gaga", email: "email@email.com", password: "******" phone_number: "087483756234")
+
+event1= Event.new(name:"event name", start_date: Time.now, end_date: Time.now - 1.hour, location:"the lawn", description:"event description", user_id: 1 )
