06c91d51a9375620ca7de60fbd56fdb85af77884
Neringa Daniulaityte <38948021+ndaniula@users.noreply.github.com>
2018-08-09 13:52:58 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-
-devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get '/dashboard', to: 'users#dashboard', as: 'dashboard'
   resources :events do
     resources :event_logs, only: [:show]
