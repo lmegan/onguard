9e0e3476cc5d4e37e19b72e3d5837f34c70538bf
tanguy Colou <tanguy.colou@skema.edu>
2018-08-09 12:08:50 +0800

    emer contact controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only: [:dashboard] do
-    resources :emergency_contacts
+  resources :users, only: [:show] do
     resources :events do
       resources :event_logs, only: [:show]
     end
   end
-
+  get "dashboard", to: "users#dashboard", as: "user_dashboard"
+  resources :emergency_contacts
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,16 @@
 # #
 # #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 # #   Character.create(name: 'Luke', movie: movies.first)
+
 EmergencyContact.destroy_all
+User.destroy_all
 
-emergencycontact1 = EmergencyContact.create(first_name: 'hey', last_name: 'ho', phone_number: 'heoo')
-emergencycontact2 = EmergencyContact.create(first_name: 'heyy', last_name: 'hoo', phone_number: 'heoooooo')
+user1 = User.new(first_name: "Lady", last_name: "Gaga", email: "email@email.com", password: "******", phone_number: "087483756234")
+
+emergencycontact1 = EmergencyContact.create(first_name: 'hey', last_name: 'ho', phone_number: 'heoo', user: user1)
+emergencycontact2 = EmergencyContact.create(first_name: 'heyy', last_name: 'hoo', phone_number: 'heoooooo', user: user1)
 
 
-user1 = User.new(first_name: "Lady", last_name: "Gaga", email: "email@email.com", password: "******" phone_number: "087483756234")
 
-event1= Event.new(name:"event name", start_date: Time.now, end_date: Time.now - 1.hour, location:"the lawn", description:"event description", user_id: 1 )
+event1= Event.new(name:"event name", start_date: Time.now, end_date: Time.now - 1.hour, location:"the lawn", description:"event description", user: user1 )
 
