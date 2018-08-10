c99df9d6d5b3822f15528c38e9eee8552e4ec6dd
Leanne Egan <lmegan92@gmail.com>
2018-08-10 09:17:15 +0800

    started on user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def dashboard
+    @events = current_user.events
+    @event = Event.new
+    @user = current_user
   end
 end
