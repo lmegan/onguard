b6cd5509e1525f8f7e41bc56728370408c5fda11
Neringa Daniulaityte <neringa.daniulaityte@gmail.com>
2018-08-13 11:34:17 +0800

    locaiton autocomplete added to the form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,27 +3,6 @@ import "bootstrap";
 // import "../components/video_home_page"
 
 
-function offset(el) {
-  let rect = el.getBoundingClientRect(),
-  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-}
-
-const sectionOne = offset(document.getElementById("one"));
-const sectionTwo = offset(document.getElementById("two"));
-const sectionThree = offset(document.getElementById("three"));
-console.log(Math.round(sectionTwo.top))
-
-window.addEventListener('scroll', () => {
-
-
-  if (window.scrollY == Math.round(sectionTwo.top)) {
-    console.log("entering 2")
-  }
-
-
-});
 
 import flatpickr from "flatpickr";
 
