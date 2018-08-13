d5e5f2952bf6cbb4e56216c06b9d1f6e92d481e9
Neringa Daniulaityte <neringa.daniulaityte@gmail.com>
2018-08-13 10:47:55 +0800

    fixing problems

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [ :edit, :update, :destroy, :deactivate]
   before_action :set_event, only: [:show, :edit, :update, :destroy, :deactivate]
 
   def index
