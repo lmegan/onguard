cff2e42af55d7ed808b18bf26524eb330e478462
tanguy Colou <tanguy.colou@skema.edu>
2018-08-16 18:30:19 +0800

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,8 @@ class User < ApplicationRecord
     #user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
     user_params = user_params.to_h
 
+
+
     user = User.find_by(provider: auth.provider, uid: auth.uid)
       user ||= User.find_by(email: auth.info.email) # User did a regular sign up in the past.
       if user
